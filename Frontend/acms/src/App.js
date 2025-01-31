f97eba299d75f1901648d7a1716c811f5551211d
pankajshetake07 <140935206+pankajshetake07@users.noreply.github.com>
2025-01-31 14:42:13 +0530

    Added admin dashboard

--- Frontend/acms/src/App.js
+++ Frontend/acms/src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import '../src/styles/style.css';
 import About from "./components/About";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Login from "./components/Login";
-import AdminHome from "./components/AdminHome";
 import AssistantDoctorHome from "./components/AssistantDoctorHome";
 import ReceptionistHome from "./components/receptionistHome";
 import { useSelector } from "react-redux";
@@ -13,17 +12,24 @@ import Logout from "./components/logout";
 import Register from './components/Register'
 import PatientDashboard from "./components/PatientDashboard";
 import DoctorDashboard from "./components/DoctorDashboard";
+import AdminEmployeeDashboard from "./components/AdminEmployeeDashboard";
 
 
 function App() {
 
   //initial state of logged
-  const myState = useSelector((state) => state.logged)
+  const logged = useSelector((state) => state.logged);
+  const [myState, setMystate] = useState(false) //useSelector((state) => state.logged)
+
+  useEffect(() => {
+    setMystate(logged)
+  }, [])
 
 
   return (
     <div className="App">
       <div style={{ display: myState.loggedIn ? "none" : "block" }}>
+        {console.log(myState.loggedIn)}
         {/* Navigation Bar */}
         <nav style={styles.navBar}>
           <ul style={styles.navList}>
@@ -44,9 +50,9 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />}> </Route>
         <Route path="/register" element={<Register />} />
-        <Route path="admin_home" element={<AdminHome />} />
+        <Route path="admin_home" element={<AdminEmployeeDashboard />} />
         <Route path="doctor_home" element={<DoctorDashboard />} />
-        <Route path="patient_home" element={<PatientDashboard />} / >
+        <Route path="patient_home" element={<PatientDashboard />} />
         <Route path="assistance_doctor_home" element={<AssistantDoctorHome />} />
         <Route path="receptionist_home" element={<ReceptionistHome />} />
         <Route path="/logout" element={<Logout />} />
